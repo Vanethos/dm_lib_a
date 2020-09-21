96e37fbaa8b6000765f2776b654e8d7101809361
Gonçalo Palma <solid.goncalo@gmail.com>
2020-09-21 12:11:19 +0400

    Dependency Manager Dependencies

--- lib/lib_a.dart
+++ lib/lib_a.dart
@@ -1,5 +1,11 @@
 library lib_a;
 
+import 'package:lib_c/lib_c.dart';
+
 class LibraryA {
-  String get name => "Library A";
+  final LibraryC libC;
+
+  LibraryA(this.libC);
+  
+  String get name => "Library A which depends on: ${libC.name}";
 }
